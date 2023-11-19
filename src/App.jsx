49ac3bf41e403f6164429ab9c944ee6720ee1f32
phong.mv <maiphong1796@gmail.com>
2023-11-19 15:41:21 +0700

    Update theme MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
+      <ModeToggle/>
       <div className="app-container">Jin Dev</div>
       <Button variant="contained">Hello world</Button>
-      <AccessAlarmIcon/>
-      <ThreeDRotation/>
     </>
   )
 }
