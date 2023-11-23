668614bf63e42e92029cf56aaec17931557ad6f6
phong.mv <maiphong1796@gmail.com>
2023-11-23 20:49:52 +0700

    Update theme mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
 import Button from '@mui/material/Button'
 import { useColorScheme } from '@mui/material/styles'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
 
-function ModeToggle() {
+function ModeSelect() {
   const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    let selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
   return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="demo-select-small-label">Mode</InputLabel>
+      <Select
+        labelId="dark-light-mode-select-label"
+        id="dark-light-mode-select"
+        value={mode}
+        label="Model"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">Light</MenuItem>
+        <MenuItem value="dark">Dark</MenuItem>
+        <MenuItem value="system">System</MenuItem>
+      </Select>
+    </FormControl>
   )
 }
 
 function App() {
   return (
     <>
-      <ModeToggle/>
-      <div className="app-container">Jin Dev</div>
-      <Button variant="contained">Hello world</Button>
+      <ModeSelect/>
     </>
   )
 }
